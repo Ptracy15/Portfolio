f5ac999fdf576d809e77e970ec19ee1c5d7eff77
Paul Tracy <ptracy16@gmail.com>
2017-03-14 17:11:23 +0000

    add thumbnails and project links

--- app.js
+++ app.js
@@ -7,7 +7,21 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 
+app.get("/", function(req, res){
+    res.render("home");
+});
 
+app.get("/mywork", function(req, res){
+    res.render("mywork");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.get("/contact", function(req, res){
+    res.render("contact");
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Started");
