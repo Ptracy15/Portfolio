ae2da441167340701cf1fd411bb7a384325e2db3
Paul Tracy <ptracy16@gmail.com>
2017-03-30 20:08:38 +0000

    update portfolio

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + "/public"));
 
 
 app.get("/", function(req, res){
-    res.render("home");
+    res.render("betterPortfolio");
 });
 
 app.get("/mywork", function(req, res){
